faa1555c5c6482d4baaa3e9337aa528ec244f98e
Soumaya LAAJAJ <soumaya.laajaj@hotmail.fr>
2024-08-21 10:20:53 +0200

    add code

---
+++ Annuaire/src/main/java/fr/isika/cda27/projet1/Annuaire/SystemInfo.java
@@ -0,0 +1,13 @@
+package fr.isika.cda27.projet1.Annuaire;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
---
+++ Annuaire/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module fr.isika.cda27.projet1.Annuaire {
+    requires javafx.controls;
+    exports fr.isika.cda27.projet1.Annuaire;
+}
