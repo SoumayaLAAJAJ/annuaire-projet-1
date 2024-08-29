ca25c17ac7df14022f3c11ee3d4285c8e21d4443
SoumayaLAAJAJ <91741200+SoumayaLAAJAJ@users.noreply.github.com>
2024-08-29 14:16:42 +0200

    Add file tests

---
+++ Annuaire/src/main/java/fr/isika/cda27/projet1/Annuaire/back/Tests.java
@@ -0,0 +1,24 @@
+package fr.isika.cda27.projet1.Annuaire.back;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Tests {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		Tree tree = new Tree();
+		//tree.createBinfile();
+//		for (int i = 0; i < 1704; i++) {
+//
+//			System.out.println((tree.readNode(tree.getRaf(), i*88).getIntern().getName()));
+//		}
+
+		System.out.println("OOOOOOOOOOOOOOOOOOOOO" + tree.getInterns().size() + "OOOOOOOOOOOOOOOOOOOOO");
+		for (Intern intern : tree.getInterns()) {
+			System.out.println("\n" + intern.getName());
+		}
+
+	}
+
+}
